0d57d555078f34305db8be64ff139f6ee13131c6
namedmichi <j.technology@web.de>
2023-02-28 20:53:30 +0100

    sync

--- zeit.sh
+++ zeit.sh
@@ -1,6 +1,5 @@
-st=$(date "+%H")
-st=${st:1:1}
-min=$(date "+%M")
+st=$(date "+%-H")
+min=$(date "+%-M")
 
 x=$1
 if  [ -z $x ]; then
@@ -12,9 +11,8 @@ timemin=$(( $st * 60 + $min ))
 timemin=$(( $timemin + $x ))
 
 st=$(( $timemin / 60 ))
-minutes=$(( $timemin % 60 ))
-
-
+st=$(( $st % 24 ))
+min=$(( $timemin % 60 ))
 
 echo "Alte uhrzeit: " $(date "+%H:%M:%S")
-echo "Neue Uhrzeit: " $st ":" $minutes
+echo "Neue Uhrzeit: " $st ":" $min
